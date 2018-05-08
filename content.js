6e81bbf882b64119c651a00e398383d3e06c80d7
mrgawde <mehulgawdenxt@gmail.com>
2018-05-08 13:16:16 +0530

    feat: Miscellenous updates

--- content.js
+++ content.js
@@ -2,10 +2,10 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "set_cookie") {
-            document.cookie = "username=BTC";
+            document.cookie = "Disable Analytics=true";
             
         }else{
-            document.cookie = "username=";
+            document.cookie = "Disable Analytics=false";
         }
     }
 );
