11878dcc23b81f8fd22c599d900124b89a1fdeae
curiosity <mehulgawdenxt@gmail.com>
2018-05-12 15:43:19 +0530

    some changes

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 // content.js
+const d1= new Date()
+d1.setDate(32);
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "set_cookie") {
-            document.cookie = "disable_analytics=true";
+            document.cookie = "disable_analytics=true; expires="+d1.toUTCString()+" UTC";
 
         } else {
-            document.cookie = "disable_analytics=false";
+            document.cookie = "disable_analytics=false; expires="+d1.toUTCString()+" UTC";
         }
     }
 );
